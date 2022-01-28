29cdb561c73632b1f4f294b88caa735b282ad370
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-28 22:26:27 +0530

    Update E2_data_info.java

--- src/E2_data_info.java
+++ src/E2_data_info.java
@@ -5,23 +5,23 @@ import java.util.regex.Pattern;
 
 public class E2_data_info {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");                                                                                // oracle.jdbc.oracledriver
+        Class.forName("com.mysql.cj.jdbc.Driver");                                                              //---------oracle.jdbc.oracledriver
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/E2data_09","root","");
-        PreparedStatement ps = con.prepareStatement("insert into info_09 values (?,?)");                         // used to insert values that full-fill the  condition
+        PreparedStatement ps = con.prepareStatement("insert into info_09 values (?,?)");                       //----------used to insert values that full-fill the  condition
 
-        Scanner sc = new Scanner(System.in);                                             // creating the object of string class
-        System.out.println("Enter the String:");                                         // a msg to  enter the string
-        String str = sc.nextLine();                                                      // for entering the string
+        Scanner sc = new Scanner(System.in);                                             //---------------creating the object of string class
+        System.out.println("Enter the String:");                                         //---------------a msg to  enter the string
+        String str = sc.nextLine();                                                      //---------------for entering the string
 
-        Pattern p_no = Pattern.compile("(0|91)?[96][0-9]{9}");                                 // format of phone no
-        Pattern email = Pattern.compile("[a-zA-Z0-9._]+@(gmail|yahoo|rediffmail)+.com");        // format of e-mail
+        Pattern p_no = Pattern.compile("(0|91)?[96][0-9]{9}");                           //---------------format of phone no
+        Pattern email = Pattern.compile("[a-zA-Z0-9._]+@(gmail|yahoo|rediffmail)+.com");   //---------------format of e-mail
 
-        Matcher m = email.matcher(str);                                                 // check the match of email in the string
-        Matcher n = p_no.matcher(str);                                                  // check the match of phone no in the string
-      //  int c=0,ctr=0;                                                                // no need just ....
-        String s1 = null ,s2 = null;                                                    // initialises two string with null value
+        Matcher m = email.matcher(str);                                                 //--------------check the match of email in the string
+        Matcher n = p_no.matcher(str);                                                  //--------------check the match of phone no in the string
+      //  int c=0,ctr=0;                                                                //--------------no need just ....
+        String s1 = null ,s2 = null;                                                    //--------------initialises two string with null value
 
-        try                                                                            // try block for the exception
+        try                                                                             //--------------try block for the exception
         {
             while(m.find())
             {
